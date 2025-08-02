d92cb23f9196ddd57484dac7278ffaec45e4ee33
Manuel Quicaia <155465120+Quicaia@users.noreply.github.com>
2025-08-02 18:43:45 +0100

    upd Header

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+
+//========================Open Menu Mobile ======================//
+
+const menuBtn = document.getElementById("menu-btn")
+const menuMobile = document.getElementById("nav-links")
+const menuBtnIcon = document.querySelector("i")
+
+menuBtn.addEventListener('click', (e)=> {
+       menuMobile.classList.toggle("open")
+    
+       //alterar o icon men
+      const isOpen = menuMobile.classList.contains("open");
+      menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line")
+});
+
+menuMobile.addEventListener("click", (e) => {
+    menuMobile.classList.remove("open");
+})
